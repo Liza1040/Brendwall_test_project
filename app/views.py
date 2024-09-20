cfcc249e43b532e9baaa3b57b6613075416df407
Liza1040 <maxsimova2002eliza@yandex.ru>
2024-09-20 19:39:19 +0300

    add data verification

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 from .models import Product
@@ -12,7 +13,7 @@ def main(request):
 
 @api_view(['POST'])
 def create_product(request):
-    print("hi", request.data)
+    validate_data(request)
     serializer = ProductSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -24,3 +25,9 @@ def list_products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+def validate_data(request):
+    if request.data["price"] == "" or request.data["name"] == "" or request.data["description"] == "":
+        raise ValidationError('Поля не должны быть пустыми')
+    elif int(request.data["price"]) <= 0:
+        raise ValidationError('Цена должна быть положительным числом')
